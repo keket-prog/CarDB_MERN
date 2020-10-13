e9d0c9386ce7b0ed3f7ee37dc12828224691b8c8
Kekelax <keketohlang@gmail.com>
2020-10-13 22:55:38 +1100

    updates

---
+++ models/car.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const carSchema = new mongoose.Schema({
+  Model: {
+    type: String,
+    required: true,
+  },
+  Make: {
+    type: String,
+    required: true,
+  },
+  Colour: {
+    type: String,
+    required: false,
+  },
+
+  RegNo: {
+    type: String,
+    required: false,
+  },
+  Owner: {
+    type: String,
+    required: false,
+  },
+  Address: {
+    type: String,
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Car", carSchema);
---
+++ routes/cars.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+
+const allCars = require("../controllers/carControllers");
